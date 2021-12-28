fb5e6cf795526a6361e355b7cd0e9f1e80fab047
Java Software <pj.midgard@yahoo.com>
2021-12-28 14:00:38 +0000

    Update Spring-77.py

--- Spring-77.py
+++ Spring-77.py
@@ -569,7 +569,7 @@ class compression:
                                         
                                         if  Circle_times2==T:
                                         
-                                            if C==1 and T8!=0:
+                                            if C==1 and T!=0:
                                             	sda17=bin(T7)[3:]
                                             	lenf=len(sda17)
                                             	szx=""
@@ -582,7 +582,7 @@ class compression:
                                             	            	z=z+1
                                             	sda17=szx+sda17
                                     										
-                                            if C==1 and T8==0:
+                                            if C==1 and T==0:
                                                     sda17=sda3[48:]
 
 
